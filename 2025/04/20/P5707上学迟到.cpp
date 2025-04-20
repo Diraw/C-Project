56d853c3e8f10bbd9ad866f84272a875dc5b8997
Diraw <craftyup7567@outlook.com>
2025-04-20 18:03:30 +0800

    20250420

--- 2025/04/20/P5707上学迟到.cpp
+++ 2025/04/20/P5707上学迟到.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
@@ -8,8 +9,18 @@ int main()
     int s, v;
     cin >> s >> v;
     int t = ceil(1.0 * s / v) + 10;
+
+    t = 8 * 60 - t;
+
+    if (t < 0)
+    {
+        t += 24 * 60;
+    }
+
     int h = t / 60;
     int m = t % 60;
-    cout << 8 - h << ":" << 60 - m << endl;
+
+    cout << setfill('0') << setw(2) << h << ":" << setfill('0') << setw(2) << m << endl;
+
     return 0;
 }
